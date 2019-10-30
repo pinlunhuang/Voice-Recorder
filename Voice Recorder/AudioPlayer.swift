e2fc82362fea7c8596d687899a1d81088020edae
Pinlun <lunwaac@gmail.com>
2019-10-30 17:18:15 +0800

    Preparing the AudioPlayer

---
+++ Voice Recorder/AudioPlayer.swift
@@ -0,0 +1,56 @@
+//
+//  AudioPlayer.swift
+//  Voice Recorder
+//
+//  Created by Pinlun on 2019/10/30.
+//  Copyright © 2019 Pinlun. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+import Combine
+import AVFoundation
+
+class AudioPlayer: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    
+    let objectWillChange = PassthroughSubject<AudioPlayer, Never>()
+    
+    var isPlaying = false {
+        didSet {
+            objectWillChange.send(self)
+        }
+    }
+    
+    var audioPlayer: AVAudioPlayer!
+    
+    func startPlayback(audio: URL) {
+        let playbackSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try playbackSession.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        } catch {
+            print("Playing over the device's speakers failed")
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audio)
+            audioPlayer.delegate = self
+            audioPlayer.play()
+            isPlaying = true
+        } catch {
+            print("Playback failed.")
+        }
+    }
+    
+    func stopPlayback() {
+        audioPlayer.stop()
+        isPlaying = false
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            isPlaying = false
+        }
+    }
+}
--- Voice Recorder/RecordingsList.swift
+++ Voice Recorder/RecordingsList.swift
@@ -25,10 +25,28 @@ struct RecordingRow: View {
     
     var audioURL: URL
     
+    @ObservedObject var audioPlayer = AudioPlayer()
+    
+    
     var body: some View {
         HStack {
             Text("\(audioURL.lastPathComponent)")
             Spacer()
+            if audioPlayer.isPlaying == false {
+                Button(action: {
+                    self.audioPlayer.startPlayback(audio: self.audioURL)
+                }) {
+                    Image(systemName: "play.circle")
+                        .imageScale(.large)
+                }
+            } else {
+                Button(action: {
+                    self.audioPlayer.stopPlayback()
+                }) {
+                    Image(systemName: "stop.fill")
+                        .imageScale(.large)
+                }
+            }
         }
     }
 }
