77d98617b7a1d268d5229ea91e8e88c7e8c158a6
Pinlun <lunwaac@gmail.com>
2019-10-30 16:01:31 +0800

    Initial Commit

---
+++ Voice Recorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Voice Recorder
+//
+//  Created by Pinlun on 2019/10/30.
+//  Copyright © 2019 Pinlun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
